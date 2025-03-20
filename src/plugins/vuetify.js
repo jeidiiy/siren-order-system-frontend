8d1ee181fd04c8db94adffa4a7a3685756d65b79
JIYONG JUNG <jeidiiy98@gmail.com>
2025-03-20 20:04:53 +0900

    #5 - feat: 기본 테마 다크 -> 라이트로 변경

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -7,6 +7,6 @@ import { createVuetify } from 'vuetify'
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
-    defaultTheme: 'dark',
+    defaultTheme: 'light',
   },
 })
