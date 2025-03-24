99d88abf35526190443fe5941eec39d236f322c4
JIYONG JUNG <jeidiiy98@gmail.com>
2025-03-24 16:05:59 +0900

    #13 - feat: 마운트 시 로그인 상태 처리
    새로고침 시 스토어 상태가 초기화되어
    기존 로그인 유저의 상태를 유지할 필요가 있음.
    컴포넌트가 마운트 될 때 쿠키에 있는 Refresh Token을 활용해
    기존 로그인 유저의 정보를 스토어에 저장하는 방식으로 해결함.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
-  <v-app>
+  <v-app class="bg-surface-variant">
     <router-view />
   </v-app>
 </template>
 
 <script setup>
-  //
+import {onMounted} from "vue";
+import {useAppStore} from "./stores/app";
+const store = useAppStore();
+onMounted(() => {
+  store.silentRefresh();
+});
 </script>
