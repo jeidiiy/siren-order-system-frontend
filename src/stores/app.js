e14771585ceaed1d86c2f661840bf449edbf78d7
JIYONG JUNG <jeidiiy98@gmail.com>
2025-03-24 20:26:12 +0900

    fix: 스토어 관련 오타 및 버그 수정 (#18)

--- src/stores/app.js
+++ src/stores/app.js
@@ -6,11 +6,7 @@ export const BEARER_PREFIX = "Bearer ";
 
 export const useAppStore = defineStore('app', {
   state: () => ({
-    user: {
-      username: null,
-      nickname: null,
-      realname: null
-    },
+    user: {username: null, nickname: null, realname: null},
     accessToken: null,
   }),
 
@@ -20,7 +16,7 @@ export const useAppStore = defineStore('app', {
         const res = await api.post("/api/v1/users/authenticate", {username, password});
         this.accessToken = res.headers['authorization'].substring(BEARER_PREFIX.length);
         this.user.username = username;
-        api.defaults.headers.common['Authorization'] = `${BEARER_PREFIX + this.token}`;
+        api.defaults.headers.common['Authorization'] = `${BEARER_PREFIX + this.accessToken}`;
         return true;
       } catch (error) {
         // TODO: 에러 처리 필요
@@ -32,7 +28,8 @@ export const useAppStore = defineStore('app', {
       try {
         await api.post("/api/v1/logout");
         this.accessToken = null;
-        this.user = null;
+        this.user.username = null;
+        api.defaults.headers.common['Authorization'] = null;
         return true;
       } catch (error) {
         // TODO: 에러 처리 필요
@@ -45,10 +42,10 @@ export const useAppStore = defineStore('app', {
         const res = await api.post('/api/v1/refresh-token');
         this.accessToken = res.headers['authorization'].substring(BEARER_PREFIX.length);
         this.user.username = decodeJWT(this.accessToken).payload.sub;
-        api.defaults.headers.common['Authorization'] = `${BEARER_PREFIX + this.token}`;
+        api.defaults.headers.common['Authorization'] = `${BEARER_PREFIX + this.accessToken}`;
         return true;
       } catch (error) {
-        // TODO: 에러처리 필요
+        // TODO: 에러 처리 필요
         return false;
       }
     }
