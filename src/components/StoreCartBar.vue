7120bc30ff3ce7142158448794d79bc87e6e9ec5
JIYONG JUNG <jeidiiy98@gmail.com>
2025-03-25 14:24:17 +0900

    #5 - feat: StoreCartBar 개발 및 메인 페이지 적용

---
+++ src/components/StoreCartBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app-bar
+    elevation="0"
+    color="primary"
+    location="bottom"
+  >
+    <v-container
+      class="wrapper d-flex justify-space-between align-center"
+    >
+      <div
+        class="button-wrapper"
+      >
+        <v-btn
+          class="text-h6"
+          width="100%"
+          @click="$router.push('/stores')"
+        >
+          여기를 클릭해서 주문할 매장을 선택해주세요
+        </v-btn>
+      </div>
+      <v-btn>
+        <v-icon>mdi-basket-plus-outline</v-icon>
+      </v-btn>
+    </v-container>
+  </v-app-bar>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+.button-wrapper {
+  width: 100%;
+}
+</style>
