50d0240773e942ae5590b74ecd99636327d1568b
JIYONG JUNG <jeidiiy98@gmail.com>
2025-03-26 01:34:25 +0900

    feat: 컴포넌트 레이아웃 구조 변경 (#30)
    * #29 - chore: CategoryView -> TypeView 로 이름 변경
    
    * #29 - DefaultLayout으로 CategorySelector 이동
    
    * #29 - 매장 레이아웃 개발 및 라우터에 적용

--- src/layouts/StoreLayout.vue
+++ src/layouts/StoreLayout.vue
@@ -2,11 +2,9 @@
   <AppHeader />
   <v-main>
     <v-container class="wrapper pa-0">
-      <CategorySelector />
       <router-view />
     </v-container>
   </v-main>
-  <StoreCartBar />
 </template>
 
 <script setup>
