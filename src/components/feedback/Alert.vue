c26a4a8de73098b72f43b418b44af66328f249b2
JIYONG JUNG <jeidiiy98@gmail.com>
2025-04-07 22:28:43 +0900

    #42 - 알럿 컴포넌트 개발

---
+++ src/components/feedback/Alert.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-alert
+    v-model="alert"
+    class="alert"
+    close-label="Close Alert"
+    closable
+    title="알림"
+    :color="`${type}`"
+    :icon="`$${type}`"
+    :type="type"
+  >
+    <v-progress-linear
+      class="position-absolute top-0 left-0 border-t-4"
+      color="green"
+      height="4"
+      :model-value="progress"
+    />
+    {{ msg }}
+  </v-alert>
+</template>
+
+<script setup>
+import useAlertStore from "@/stores/alert";
+
+const alertStore = useAlertStore();
+const {alert, msg, type, progress} = storeToRefs(alertStore);
+</script>
+
+<style lang="scss" scoped>
+.alert {
+  position: absolute; top: 30px; right: 30px; z-index: 2000;
+}
+</style>
